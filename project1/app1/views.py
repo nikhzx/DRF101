830e8060a8ec1a65ce28849e90d193b54231e379
Nikhil Jaiswal <nikhzx@gmail.com>
2024-03-21 00:35:16 +0530

    Changes on 20th March, Serializers made

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+# Model Object - Single Student DATA
+def student_detail(request, pk):
+    stu = Student.objects.get(id=pk)
+    serializer = StudentSerializer(stu)
+    # data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(data, content_type = 'application/json')
+    return JsonResponse(serializer.data)
+
+#QuerySet - All Student Data
+def students_details(request):
+    stu = Student.objects.all()
+    serializer = StudentSerializer(stu, many = True)
+    # data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(data, content_type = 'application/json')
+    return JsonResponse(serializer.data, safe= False)
