503f93b68cf70a46f8ea413975a42a552a9b8424
Nikhil Jaiswal <nikhzx@gmail.com>
2024-03-27 22:36:33 +0530

    get api developed

--- crudproject/app/views.py
+++ crudproject/app/views.py
@@ -1,3 +1,27 @@
+import http
 from django.shortcuts import render
+from requests import request
+import io
+from rest_framework.parsers import JSONParser
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 # Create your views here.
+def student_api(request):
+    if request.method == "GET":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id', None)
+        if id is not None :
+            data = Student.objects.get(id = id)
+            serializer = StudentSerializer(data)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type = 'application/json')
+        else:  
+            data = Student.objects.all()
+            serializer = StudentSerializer(data, many = True)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type = "application/json")
